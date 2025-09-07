a5439e3919efb15ee4edb533762b2cb9d498e150
ArmouredScorpi0 <armouredscorpio@gmail.com>
2025-09-07 05:55:02 +0000

    greyfix

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // This 'define' block is the key to fixing the Firebase connection.
-  // It tells Vite to find your VITE_FIREBASE_CONFIG environment variable
-  // and make it available inside the application code under the name `__firebase_config__`.
   define: {
     '__firebase_config__': process.env.VITE_FIREBASE_CONFIG,
   }
